e688042b7f1f3b91e40151431659ce44921317e5
jiyeoon <jypark2781@gmail.com>
2024-03-17 10:27:56 +0900

    feat: type 추가 및 변경

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -1,3 +1,2 @@
 export type str = string | null;
 export type num = string | null;
-export type coinFile = { file: File; block: String; timeStamp: String };
--- src/interfaces/upload/index.ts
+++ src/interfaces/upload/index.ts
@@ -1,9 +1,15 @@
-import { str, num, coinFile } from "@/interfaces/common";
+import { str, num } from "@/interfaces/common";
 
+export type coinFile = { file: File; block: String; timeStamp: String };
 export type uploadValid = {
     ticker: str;
     contract: str;
     price: num;
     files: coinFile[];
+};
+export type filesdValid = {
+    block: str;
+    timeStamp: str;
+    fine: File;
 };
 export type uploadRes = { result?: str; message?: str };
